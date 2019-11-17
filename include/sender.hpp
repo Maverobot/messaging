93caa470e628d8e93c683de03185634ffa2b0337
Maverobot <quzhengrobot@gmail.com>
2019-11-17 10:59:14 +0100

    ADD: exception if queue does not exist

--- include/sender.hpp
+++ include/sender.hpp
@@ -8,10 +8,11 @@ class Sender {
   explicit Sender(Queue* q) : q_(q) {}
 
   template <typename T>
-  void send(T const& msg) {
-    if (q_) {
-      q_->push(msg);
+  void send(T&& msg) {
+    if (!q_) {
+      throw std::logic_error("Sender with no queue cannot send.");
     }
+    q_->push(std::forward<T>(msg));
   }
 };
 }  // namespace messaging
